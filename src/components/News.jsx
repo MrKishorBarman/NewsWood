4a6cfd468426dbbceec74a0a700d280b927e15d0
Kishor <kishor3barman@gmail.com>
2024-10-23 22:09:56 +0530

    resolved

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -37,7 +37,7 @@ const News = ({ category, setProgress }) => {
   const loadAllArticles = useCallback(async () => {
     try {
       setLoading(true);
-      const url = `../public/JSON/${category}.json`; 
+      const url = `../JSON/${category}.json`; 
       let data = await fetch(url);
 
       if (!data.ok) {
